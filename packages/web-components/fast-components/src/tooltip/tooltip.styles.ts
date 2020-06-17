04a9520c8d402cc28c2b6d1fef4f6a9fcd6ae2e4
Stephane Comeau <scomea@microsoft.com>
2020-06-17 09:54:46 -0700

    working

--- packages/web-components/fast-components/src/tooltip/tooltip.styles.ts
+++ packages/web-components/fast-components/src/tooltip/tooltip.styles.ts
@@ -3,7 +3,22 @@ import { css } from "@microsoft/fast-element";
 export const TooltipStyles = css`
     :host {
         contain: layout;
-        display: block;
         overflow: visible;
+        height: 0;
+        width: 0;
     }
+
+    .tooltip {
+        background-color: grey;
+        padding: 10px;
+        display: inline-block;
+        height: fit-content;
+        width: fit-content;
+    }
+    
+    :host(.top) .tooltip {
+        background-color: red;
+        margin: 20px;
+    }
+    
 `;
--- packages/web-components/fast-foundation/src/tooltip/tooltip.ts
+++ packages/web-components/fast-foundation/src/tooltip/tooltip.ts
@@ -1,36 +1,64 @@
 import { attr, DOM, FASTElement, observable } from "@microsoft/fast-element";
 import {AnchoredRegion} from "../anchored-region"
 
-export type TooltipPosition = "top" | "right" | "bottom | left";
+export type TooltipPosition = "top" | "right" | "bottom" | "left";
 
 export class Tooltip extends FASTElement {
     @attr({ mode: "boolean" })
     public hidden: boolean = false;
+    private hiddenChanged(): void {
+
+    }
 
     @attr       
     public anchor: string = "";
     private anchorChanged(): void {
+        if (this.isConnected){
+            this.updateLayout();
+        }
     }
 
     @attr
     public delay: number = 300;
 
     @attr
-    public position: TooltipPosition = "top";
+    public position: TooltipPosition | null = null;
+    private positionChanged(): void {
+        if (this.isConnected){
+            this.updateLayout();
+        }
+    }
 
     @observable
     public anchorElement: HTMLElement | null = null;
-    // private anchorElementChanged(): void {
-    //     if (this.initialLayoutComplete) {
-    //         this.initialLayoutComplete = false;
-    //         this.reset();
-    //     }
-    // }
+    private anchorElementChanged(): void {
+        if (this.isConnected){
+            this.updateLayout();
+        }
+    }
+
+    @observable
+    public verticalPositioningMode: string = "dynamic";
+
+    @observable
+    public horizontalPositioningMode: string = "dynamic";
+
+    @observable
+    public horizontalInset: string = "false";
+
+    @observable
+    public verticalInset: string = "false";
+
+    @observable
+    public verticalDefaultPosition: string | undefined = undefined;
+
+    @observable
+    public horizontalDefaultPosition: string | undefined = undefined;
 
     /**
      * reference to the component root
      */
-    public root: HTMLDivElement;
+    public tooltipRoot: HTMLDivElement;
 
     /**
      * reference to the anchored region
@@ -41,15 +69,86 @@ export class Tooltip extends FASTElement {
 
     constructor() {
         super();
+        this.classList.toggle("testclass", true);
     }
 
     public connectedCallback(): void {
         super.connectedCallback();
-        this.region.viewportElement = this.root.parentElement;
+        this.region.region.addEventListener("change", this.handlePositionChange);
+        this.region.viewportElement = this.tooltipRoot.parentElement;
+        this.updateLayout();
+        this.updatePositioningStyles((this.region as any).$fastController.element as AnchoredRegion);
     }
 
     public disconnectedCallback(): void {
+        this.region.region.removeEventListener("change", this.handlePositionChange);
         super.disconnectedCallback();
+    }
+
+    public handlePositionChange(ev: Event): void {
+        const anchoredRegion: AnchoredRegion = (ev.target as any).$fastController.element as AnchoredRegion;
+        this.classList.toggle("top", anchoredRegion.verticalPosition === "top");
+        this.classList.toggle("bottom", anchoredRegion.verticalPosition === "bottom");
+        this.classList.toggle("inset-top", anchoredRegion.verticalPosition === "insetTop");
+        this.classList.toggle("inset-bottom", anchoredRegion.verticalPosition === "insetBottom");
+
+        this.classList.toggle("left", anchoredRegion.horizontalPosition === "left");
+        this.classList.toggle("right", anchoredRegion.horizontalPosition === "right");
+        this.classList.toggle("inset-left", anchoredRegion.horizontalPosition === "insetLeft");
+        this.classList.toggle("inset-right", anchoredRegion.horizontalPosition === "insetRight");
+    }
+
+    private updatePositioningStyles = (anchoredRegion: AnchoredRegion): void => {
+        
+    }
+
+    private updateLayout(): void {
+        switch (this.position) {
+            case "top":
+                this.verticalPositioningMode = "locktodefault";
+                this.horizontalPositioningMode = "dynamic";
+                this.verticalDefaultPosition = "top";
+                this.horizontalDefaultPosition = undefined;
+                this.horizontalInset = "true";
+                this.verticalInset = "false";
+                break;
+
+            case "bottom":
+                this.verticalPositioningMode = "locktodefault";
+                this.horizontalPositioningMode = "dynamic";
+                this.verticalDefaultPosition = "bottom";
+                this.horizontalDefaultPosition = undefined;
+                this.horizontalInset = "true";
+                this.verticalInset = "false";
+                break;
+
+            case "right":
+                this.verticalPositioningMode = "dynamic";
+                this.horizontalPositioningMode = "locktodefault";
+                this.verticalDefaultPosition = undefined;
+                this.horizontalDefaultPosition = "right";
+                this.horizontalInset = "false";
+                this.verticalInset = "true";
+                break;
+
+            case "left":
+                this.verticalPositioningMode = "dynamic";
+                this.horizontalPositioningMode = "locktodefault";
+                this.verticalDefaultPosition = undefined;
+                this.horizontalDefaultPosition = "left";
+                this.horizontalInset = "false";
+                this.verticalInset = "true";
+                break;
+
+            case null:
+                this.verticalPositioningMode = "dynamic";
+                this.horizontalPositioningMode = "dynamic";
+                this.verticalDefaultPosition = undefined;
+                this.horizontalDefaultPosition = undefined;
+                this.horizontalInset = "false";
+                this.verticalInset = "false";
+                break;   
+        }
     }
 
     /**
